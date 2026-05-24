d59121bfe9071a1b5849b3592dedafe8a1d376c5
ts73103212-lang <ts73103212@gmail.com>
2026-05-24 22:43:11 +0900

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -79,7 +79,7 @@ VOL_RATIO_THRESHOLD = 1.8
 HOLDINGS = [
     {"code": "TSE:6613", "shares": 100, "cost": 1600},  # QDレーザ
     {"code": "TSE:7375", "shares": 0, "cost": 0},       # リファインバース
-    {"code": "TSE:7162", "shares": 0, "cost": 0},       # アストマックス
+    {"code": "TSE:9227", "shares": 0, "cost": 0},       # マイクロ波化学
     # ↓ 銘柄を追加する場合はここにコピー＆ペーストして編集
     # {"code": "TSE:XXXX", "shares": 100, "cost": nnnn},  # 銘柄名
     # 監視銘柄（保有なし）は"shares": 0, "cost": 0とすること。
